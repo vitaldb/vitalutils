93283bea430fab19f7fc4c2e5bce36258d78db41
이형철 <lucid80@gmail.com>
2023-11-03 09:11:18 +0900

    added read_vital

--- python/vitaldb-pypi/setup.py
+++ python/vitaldb-pypi/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
     name="vitaldb",
-    version="1.4.1",
+    version="1.4.3",
     author="Hyung-Chul Lee",
     author_email="vital@snu.ac.kr",
     description="VitalDB Python Libray",
