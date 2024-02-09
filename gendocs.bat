86c2feacc99a743d50a624ac02328b166f4124b7
이형철 <lucid80@gmail.com>
2024-02-10 01:02:02 +0900

    Updated docs

--- gendocs.bat
+++ gendocs.bat
@@ -1,4 +1,5 @@
-sphinx-apidoc -f -o docs/source/matching python/matching/ICU
-sphinx-apidoc -f -o docs/source/vitaldb python/vitaldb-pypi/vitaldb
+sphinx-apidoc -f -o docs/matching python/matching/ICU
+sphinx-apidoc -f -o docs/vitaldb python/vitaldb-pypi/vitaldb
 call %~dp0\docs\make.bat html
+start chrome "%~dp0/docs/_build/html/index.html"
 PAUSE
