081a5ad3e93e392216bf2cefa30faa1459918008
이형철 <lucid80@gmail.com>
2024-02-10 12:21:29 +0900

    updated docs

--- gendocs.py
+++ gendocs.py
@@ -1,4 +1,4 @@
 import os
 os.system("sphinx-apidoc -f -o docs/matching python/matching/ICU")
 os.system("sphinx-apidoc -f -o docs/vitaldb python/vitaldb-pypi/vitaldb")
-os.system("sphinx-build -M html docs docs/_build")
+os.system("sphinx-build docs docs/_build")
